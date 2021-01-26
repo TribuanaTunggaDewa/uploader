d773ec7023ecd3785ff019413b666b4f7ed6ae91
Farza Nurifan <farza@lenna.ai>
2021-01-26 11:20:34 +0700

    Tidy up

--- main/local.py
+++ main/local.py
@@ -1,16 +1,13 @@
 import os
-import shutil
 
 from constanta import UPLOAD_FOLDER
 
 
 def upload(request):
-    # get data
     f = request.files['file']
     folder = request.form['folder']
     path = '{}/{}'.format(UPLOAD_FOLDER, folder)
 
-    # upload file
     try:
         os.makedirs(path)
     except FileExistsError:
@@ -21,16 +18,12 @@ def upload(request):
 
 
 def delete(request):
-    # get data
     temp = request.form['path']
     path = '{}/{}'.format(UPLOAD_FOLDER, temp)
 
-    # delete folder
     try:
         os.remove(path)
     except FileNotFoundError:
         pass
-    except PermissionError:
-        shutil.rmtree(path)
 
     return {'status': 'OK'}
